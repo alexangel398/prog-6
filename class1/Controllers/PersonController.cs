ebb56b942f74e9eb2cccba2a4f7f07aed7c9f4b6
Angel <alexangel398@gmail.com>
2024-08-29 11:29:56 -0300

    class2

---
+++ class1/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace class1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+       
+        // POST api/<PersonController>
+        [HttpPost]
+        public List<Persona> GetPersonas(Persona persona)
+        {
+            List<Persona> lista = new List<Persona>();
+            for (int i = 0; i < 10;i++) 
+            {
+                lista.Add(persona);  
+            }
+            return lista;
+        }
+
+    }
+}
